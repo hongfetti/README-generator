46a9962f2a45b27cdb374069017f02e43e24171a
hongfetti <ryan.hong1994@gmail.com>
2025-01-17 12:31:56 -0500

    starter code

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 // TODO: Include packages needed for this application
+import inquirer from 'inquirer'
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile('./output/README.md', process.argv[2], (err) =>
+        err ? console.error(err) : console.log('Success!')
+      );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt({
+            type: 'input',
+            message: '',
+            name: '',
+        })
+
+}
 
 // Function call to initialize app
 init();
